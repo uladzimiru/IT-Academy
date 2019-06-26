c51dcb9add66b80ecf2ec87504297aa264ed11cf
Uladzimir Fedasenka <ufedasenka@gmail.com>
2019-06-26 22:44:38 +0300

    ready

--- menu.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    if (document.getElementById('ddmenu').style.display == 'none') {
-
-        document.getElementById('ddmenu').style.display = 'block';
-    } else {
-        document.getElementById('ddmenu').style.dislay = 'none';
-    }
-}
